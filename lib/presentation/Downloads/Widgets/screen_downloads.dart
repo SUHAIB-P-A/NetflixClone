15b643775d2ab944bb483c6088b1ab3b0a426748
thegreatcode1 <suhaibpa72@gmail.com>
2023-10-31 15:03:26 +0530

    appbar widget call in the appbar parameter

--- lib/presentation/Downloads/Widgets/screen_downloads.dart
+++ lib/presentation/Downloads/Widgets/screen_downloads.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/presentation/Widgets/app_bar_widget.dart';
 
 class ScreenDownload extends StatelessWidget {
   const ScreenDownload({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  const Scaffold(
+      appBar:  PreferredSize(
+        preferredSize: Size.fromHeight(30),
+        child: AppBarWidget()),
+     
       body: Center(
         child: Text("Downloads"),
       ),
