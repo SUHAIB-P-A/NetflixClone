9027216d1068901bfa174303d90c655c2398ceec
thegreatcode1 <suhaibpa72@gmail.com>
2023-10-31 11:42:16 +0530

    add dummy text on each screen

--- lib/presentation/Search/screen_search.dart
+++ lib/presentation/Search/screen_search.dart
@@ -5,6 +5,10 @@ class ScreenSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text("Search"),
+      ),
+    );
   }
 }
