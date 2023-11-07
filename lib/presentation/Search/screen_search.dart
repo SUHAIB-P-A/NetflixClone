ede851699d4025e9d69d136206a6b806cbc50ddf
thegreatcode1 <suhaibpa72@gmail.com>
2023-11-07 19:52:25 +0530

    add a search on the scarch screen

--- lib/presentation/Search/screen_search.dart
+++ lib/presentation/Search/screen_search.dart
@@ -1,13 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
 
 class ScreenSearch extends StatelessWidget {
   const ScreenSearch({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Search"),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              CupertinoSearchTextField(
+                prefixIcon: const Icon(
+                  CupertinoIcons.search,
+                  color: grey,
+                ),
+                suffixIcon: const Icon(
+                  CupertinoIcons.xmark_circle_fill,
+                  color: grey,
+                ),
+                backgroundColor: grey.withOpacity(0.4),
+                style: const TextStyle(color: white),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
