cf8a221684e1af3aef437377db84f1911376fbbf
thegreatcode1 <suhaibpa72@gmail.com>
2023-11-09 21:10:16 +0530

    add a pageview in funand laugh page

--- lib/presentation/FastLaugh/screen_fast_laugh.dart
+++ lib/presentation/FastLaugh/screen_fast_laugh.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/presentation/FastLaugh/widgets/contentsofscroolitem.dart';
 
 class ScreenFastLaugh extends StatelessWidget {
   const ScreenFastLaugh({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("FsatandLaughs"),
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          children: List.generate(
+            10,
+            (index) {
+              return const VideoListItem();
+            },
+          ),
+        ),
       ),
     );
   }
