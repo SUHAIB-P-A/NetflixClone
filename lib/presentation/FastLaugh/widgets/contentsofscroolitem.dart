843c0f1db9b1d511be59decb565d1b3cef52347f
thegreatcode1 <suhaibpa72@gmail.com>
2023-11-09 21:57:45 +0530

    create a column for the other widget

--- lib/presentation/FastLaugh/widgets/contentsofscroolitem.dart
+++ lib/presentation/FastLaugh/widgets/contentsofscroolitem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
 
 class VideoListItem extends StatelessWidget {
   final int index;
@@ -17,16 +18,37 @@ class VideoListItem extends StatelessWidget {
         ),
         Align(
           alignment: Alignment.bottomCenter,
-          child: Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.volume_off_outlined),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  backgroundColor: grey.withOpacity(0.4),
+                  radius: 30,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.volume_off_outlined),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CircleAvatar(),
+                    CircleAvatar(),
+                    CircleAvatar(),
+                    CircleAvatar(),
+                    CircleAvatar(),
+                  ],
+                ),
+              ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
 }
+
+
