9847911feaa89c15fb6337f9a60118852f2b7d56
thegreatcode1 <suhaibpa72@gmail.com>
2023-11-14 22:52:25 +0530

    number ui set

--- lib/presentation/home/widgets/numbercard.dart
+++ lib/presentation/home/widgets/numbercard.dart
@@ -1,4 +1,6 @@
+import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
 import 'package:netflix/core/constants/const.dart';
 
 class NumberCard extends StatelessWidget {
@@ -35,8 +37,15 @@ class NumberCard extends StatelessWidget {
         Positioned(
           left: 15,
           bottom: 1,
-          child: Text("${index+1}",
-          style: TextStyle(fontSize: 80),
+          child: BorderedText(
+            strokeWidth: 5.0,
+            strokeColor: white,
+            child: Text("${index+1}",
+            style: TextStyle(fontSize: 80,
+            fontWeight: FontWeight.bold,
+            color: backgroundcolor.withOpacity(0.9)
+            ),
+            ),
           ),
         )
       ],
