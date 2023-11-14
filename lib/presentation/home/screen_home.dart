6d192e6944b2cbd3279a29e734ebedbdfb829e8c
thegreatcode1 <suhaibpa72@gmail.com>
2023-11-14 21:45:33 +0530

    change the file o title and main card

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/core/colors/colors.dart';
 import 'package:netflix/core/constants/const.dart';
-import 'package:netflix/presentation/home/widgets/main_card.dart';
-import 'package:netflix/presentation/home/widgets/title.dart';
+import 'package:netflix/presentation/Widgets/main_card.dart';
+import 'package:netflix/presentation/Widgets/title.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
@@ -11,21 +11,34 @@ class ScreenHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const MainText(title: "Released in the past year"),
-            LimitedBox(
-              maxHeight: 230,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: List.generate(10, (index) => const MainCard()),
-              ),
-            ),
-          ],
-        ),
+        child: MainTitleCard(),
       ),
     );
   }
 }
 
+class MainTitleCard extends StatelessWidget {
+  const MainTitleCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const MainText(title: "Released in the past year"),
+        LimitedBox(
+          maxHeight: 230,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: List.generate(
+              10,
+              (index) => const MainCard(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
