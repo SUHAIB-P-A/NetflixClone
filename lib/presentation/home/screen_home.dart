0bf7cc0a9937df3aaa24d8fabc43350e83cd538d
thegreatcode1 <suhaibpa72@gmail.com>
2023-11-16 09:50:25 +0530

    extract the widget

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -3,6 +3,7 @@ import 'package:netflix/core/colors/colors.dart';
 
 import 'package:netflix/core/constants/const.dart';
 import 'package:netflix/presentation/Widgets/main_title_card.dart';
+import 'package:netflix/presentation/home/widgets/backgroundcard.dart';
 import 'package:netflix/presentation/home/widgets/bottomicon.dart';
 import 'package:netflix/presentation/home/widgets/numberwidgetcard.dart';
 
@@ -16,54 +17,7 @@ class ScreenHome extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 650,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(img),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          BottomIconHome(icon: addicon, text: "Add List"),
-                          TextButton.icon(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(white),
-                            ),
-                            onPressed: () {},
-                            icon: Icon(
-                              playicon,
-                              color: backgroundcolor,
-                            ),
-                            label: Text(
-                              'Play',
-                              style: TextStyle(
-                                color: backgroundcolor,
-                              ),
-                            ),
-                          ),
-                          BottomIconHome(
-                            icon: infoicon,
-                            text: "Info",
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              BackgroundCard(),
               MainTitleCard(
                 title: "Released in the past year",
               ),
---
+++ lib/presentation/home/widgets/backgroundcard.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/core/constants/const.dart';
+import 'package:netflix/presentation/home/widgets/bottomicon.dart';
+
+class BackgroundCard extends StatelessWidget {
+  const BackgroundCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 650,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(img),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          BottomIconHome(icon: addicon, text: "Add List"),
+                          _playbutton(),
+                          BottomIconHome(
+                            icon: infoicon,
+                            text: "Info",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+  }
+}
+
+TextButton _playbutton() {
+    return TextButton.icon(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(white),
+      ),
+      onPressed: () {},
+      icon: Icon(
+        playicon,
+        color: backgroundcolor,
+      ),
+      label: Text(
+        'Play',
+        style: TextStyle(
+          color: backgroundcolor,
+        ),
+      ),
+    );
+  }
