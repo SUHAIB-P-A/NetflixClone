c30b9dae61b4ae1f0226c953f8421370805bcdfc
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-11 20:34:12 +0530

    step 2 BE

---
+++ lib/domain/core/api_end_points.dart
@@ -0,0 +1,6 @@
+import 'package:netflix/core/constants/strings.dart';
+import 'package:netflix/infrastructure/api_key.dart';
+
+class ApiEndPoints {
+  static const endPointsDownload = "$kBaseUrl/trending/all/day?api_key=$apiKey";
+}
