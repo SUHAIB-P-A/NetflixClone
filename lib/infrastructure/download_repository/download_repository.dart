affe2dc3a3eab3fbc38f53a057715b01dfdba4ba
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-14 23:30:02 +0530

    api calling

---
+++ lib/infrastructure/download_repository/download_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:netflix/domain/core/api_end_points.dart';
+import 'package:netflix/domain/core/failures/main_failures.dart';
+import 'package:netflix/domain/downloads/i_download_repo.dart';
+import 'package:netflix/domain/downloads/models/downloads.dart';
+
+class DownloadRepository implements IDownloadRepo {
+  @override
+  Future<Either<MainFailures, List<Downloads>>> getdownloadsimages() async {
+    try {
+      final Response response =
+          await Dio(BaseOptions()).get(ApiEndPoints.endPointsDownload);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final List<Downloads> downloadlist = [];
+        for (final raw in response.data) {
+          downloadlist.add(Downloads.fromJson(raw as Map<String, dynamic>));
+        }
+        print(downloadlist);
+        return Right(downloadlist);
+      } else {
+        return const Left(MainFailures.serverFailure());
+      }
+    } catch (e) {
+      return const Left(MainFailures.clientFaliure());
+    }
+  }
+}
