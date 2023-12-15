06fe1900ec16620b0df215143f7898f7ee60478f
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-15 21:11:28 +0530

    a init function copied in the the inhuctable function

--- lib/domain/core/di/injuctable.dart
+++ lib/domain/core/di/injuctable.dart
@@ -1,7 +1,11 @@
 
+
+import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+
+final getit = GetIt.instance;
 
 @InjectableInit()
 Future<void> configinjuction()async{
-
+ await init(getit,Environment.prod);
 }
