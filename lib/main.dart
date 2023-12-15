80f8eb445e8b58cbfeae014b72984a4caf6ffdc3
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-15 22:13:49 +0530

    set the multblocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/application/downloads/downlodes_bloc.dart';
 import 'package:netflix/core/colors/colors.dart';
 import 'package:netflix/domain/core/di/injuctable.dart';
 import 'package:netflix/presentation/main_page/Widgets/main_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configinjuction();
+
   runApp(const MyApp());
 }
 
@@ -15,20 +17,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Netflix',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(backgroundColor: Colors.transparent),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        scaffoldBackgroundColor: backgroundcolor,
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: white),
-          bodyMedium: TextStyle(color: white),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => DownlodesBloc(downloadsrepo),
         ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: true,
+        title: 'Netflix',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: backgroundcolor,
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(color: white),
+            bodyMedium: TextStyle(color: white),
+          ),
+        ),
+        home: ScreenMainPage(),
       ),
-      home: ScreenMainPage(),
     );
   }
 }
