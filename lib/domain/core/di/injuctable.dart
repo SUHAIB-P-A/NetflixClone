2aa97555e4374f0346c0601be788e63b3a54e87b
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-15 21:35:08 +0530

    first step finish the injuction

--- lib/domain/core/di/injuctable.dart
+++ lib/domain/core/di/injuctable.dart
@@ -2,10 +2,12 @@
 
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:netflix/domain/core/di/injuctable.config.dart';
 
 final getit = GetIt.instance;
 
 @InjectableInit()
 Future<void> configinjuction()async{
- await init(getit,Environment.prod);
+ await getit.init(environment: Environment.prod);
 }
+
