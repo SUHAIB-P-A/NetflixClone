6b4fc8236d6c62f1ae0da65203d679740d0ff744
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-23 22:45:15 +0530

    set api end points

--- lib/domain/core/api_end_points.dart
+++ lib/domain/core/api_end_points.dart
@@ -3,4 +3,5 @@ import 'package:netflix/infrastructure/api_key.dart';
 
 class ApiEndPoints {
   static const endPointsDownload = "$kBaseUrl/trending/all/day?api_key=$apiKey";
+  static const endPointsSearch = "$kBaseUrl/search/movie?api_key=$apiKey";
 }
