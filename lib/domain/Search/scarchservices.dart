c15a4b397f65d0b4a6b15efd6a8cc31fa3881603
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-23 22:56:42 +0530

    add services similar to repocitory on downloads

---
+++ lib/domain/Search/scarchservices.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:netflix/domain/Search/models/scaech_respo/scaech_respo.dart';
+import 'package:netflix/domain/core/failures/main_failures.dart';
+
+abstract class SearchServices {
+  Future<Either<MainFailures, List<ScaechRespo>>> searchMovies({
+    required String movieQuary,
+  });
+}
