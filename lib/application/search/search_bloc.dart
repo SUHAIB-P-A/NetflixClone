639ff1390dd64b016eeb2facbb3be4bf297e35eb
thegreatcode1 <suhaibpa72@gmail.com>
2023-12-24 09:51:39 +0530

    nothing

--- lib/application/search/search_bloc.dart
+++ lib/application/search/search_bloc.dart
@@ -2,7 +2,10 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:netflix/domain/Search/models/scaech_respo/result.dart';
+import 'package:netflix/domain/downloads/i_download_repo.dart';
 import 'package:netflix/domain/downloads/models/downloads.dart';
+import 'package:netflix/infrastructure/scarchservices/searchservices.dart';
+import 'package:netflix/presentation/Search/widgets/screen_idel_scarch.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
@@ -10,9 +13,23 @@ part 'search_bloc.freezed.dart';
 
 @Injectable()
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
-  SearchBloc() : super(_Initial()) {
-    on<SearchEvent>((event, emit) {
-      // TODO: implement event handler
+  final IDownloadRepo downloadservices;
+  final Searchservices searchservies;
+  SearchBloc(
+    this.downloadservices,
+    this.searchservies,
+  ) : super(SearchState.initial()) {
+//idel state
+    on<_Initial>((event, emit) {
+      //get trending
+      downloadservices.getdownloadsimages();
+      //dispaly ui
+    });
+//search state
+    on<_SearchMovie>((event, emit) {
+      //scarch ang call api
+      searchservies.searchMovies(movieQuary: event.movieQuary);
+      //dispaly ui
     });
   }
 }
--- lib/domain/core/di/injuctable.config.dart
+++ lib/domain/core/di/injuctable.config.dart
@@ -10,13 +10,13 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:netflix/application/downloads/downlodes_bloc.dart' as _i7;
-import 'package:netflix/application/search/search_bloc.dart' as _i5;
+import 'package:netflix/application/downloads/downlodes_bloc.dart' as _i6;
+import 'package:netflix/application/search/search_bloc.dart' as _i7;
 import 'package:netflix/domain/downloads/i_download_repo.dart' as _i3;
 import 'package:netflix/infrastructure/download_repository/download_repository.dart'
     as _i4;
 import 'package:netflix/infrastructure/scarchservices/searchservices.dart'
-    as _i6;
+    as _i5;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -30,10 +30,13 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     gh.lazySingleton<_i3.IDownloadRepo>(() => _i4.DownloadRepository());
-    gh.factory<_i5.SearchBloc>(() => _i5.SearchBloc());
-    gh.lazySingleton<_i6.Searchservices>(() => _i6.Searchservices());
-    gh.factory<_i7.DownlodesBloc>(
-        () => _i7.DownlodesBloc(gh<_i3.IDownloadRepo>()));
+    gh.lazySingleton<_i5.Searchservices>(() => _i5.Searchservices());
+    gh.factory<_i6.DownlodesBloc>(
+        () => _i6.DownlodesBloc(gh<_i3.IDownloadRepo>()));
+    gh.factory<_i7.SearchBloc>(() => _i7.SearchBloc(
+          gh<_i3.IDownloadRepo>(),
+          gh<_i5.Searchservices>(),
+        ));
     return this;
   }
 }
