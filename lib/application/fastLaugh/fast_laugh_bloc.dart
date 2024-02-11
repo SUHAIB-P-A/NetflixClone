b51615933d4fbb0d3e9ac5114670626935394098
thegreatcode1 <suhaibpa72@gmail.com>
2024-02-11 23:06:45 +0530

    like 4th commit add video url on the F&L bloc code file

--- lib/application/fastLaugh/fast_laugh_bloc.dart
+++ lib/application/fastLaugh/fast_laugh_bloc.dart
@@ -8,15 +8,27 @@ part 'fast_laugh_event.dart';
 part 'fast_laugh_state.dart';
 part 'fast_laugh_bloc.freezed.dart';
 
+final video_url = [
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+  "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+];
+
 @injectable
 class FastLaughBloc extends Bloc<FastLaughEvent, FastLaughState> {
   FastLaughBloc(
     IDownloadRepo downloadserivies,
   ) : super(FastLaughState.initial()) {
     on<Initialize>((event, emit) {
-      
       //fatch data(get trending movies)
-      
+
       //send ui
     });
   }
