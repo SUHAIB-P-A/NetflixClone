2c3334b1f944a83445e281aa7ae5f149a9ffd8bb
thegreatcode1 <suhaibpa72@gmail.com>
2024-02-16 10:58:12 +0530

    10th commit for UI blocprovider add inside the WidgetsBinding.instance.addPostFrameCallback function

--- lib/presentation/FastLaugh/screen_fast_laugh.dart
+++ lib/presentation/FastLaugh/screen_fast_laugh.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/application/fastLaugh/fast_laugh_bloc.dart';
 import 'package:netflix/presentation/FastLaugh/widgets/contentsofscroolitem.dart';
 
 class ScreenFastLaugh extends StatelessWidget {
@@ -6,6 +8,9 @@ class ScreenFastLaugh extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      BlocProvider.of<FastLaughBloc>(context).add(const Initialize());
+    },);
     return Scaffold(
       body: SafeArea(
         child: PageView(
