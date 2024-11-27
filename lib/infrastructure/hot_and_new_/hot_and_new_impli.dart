b073da7d49545137ebef85c8f4e1071cf80c432c
thegreatcode1 <suhaibpa72@gmail.com>
2024-11-27 20:22:23 +0530

    set up api calling

--- lib/infrastructure/hot_and_new_/hot_and_new_impli.dart
+++ lib/infrastructure/hot_and_new_/hot_and_new_impli.dart
@@ -1,17 +1,48 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:netflix/domain/core/api_end_points.dart';
 import 'package:netflix/domain/core/failures/main_failures.dart';
 import 'package:netflix/domain/hot_and_new_respo/model/hot_and_new_respo/hot_and_new_respo.dart';
 import 'package:netflix/domain/hot_and_new_respo/model/hot_and_new_services.dart';
 
 class HotAndNewImplimentation implements HotAndNewServices {
   @override
-  Future<Either<MainFailures, HotAndNewRespo>> getHotAndNewMovieData() async{
-    
+  Future<Either<MainFailures, HotAndNewRespo>> getHotAndNewMovieData() async {
+    try {
+      final Response response = await Dio(BaseOptions()).get(
+        ApiEndPoints.endpointsHotandNewMovie,
+      );
+      log(response.data.toString());
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final hotandnewmovielist = HotAndNewRespo.fromJson(response.data);
+        return Right(hotandnewmovielist);
+      } else {
+        return const Left(MainFailures.serverFailure());
+      }
+    } catch (e) {
+      log(e.toString());
+      return const Left(MainFailures.clientFaliure());
+    }
   }
 
   @override
   Future<Either<MainFailures, HotAndNewRespo>> getHotAndNewTvData() async{
-    
+    try {
+      final Response response = await Dio(BaseOptions()).get(
+        ApiEndPoints.endpointsHotandNewTv,
+      );
+      log(response.data.toString());
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final hotandnewtvlist = HotAndNewRespo.fromJson(response.data);
+        return Right(hotandnewtvlist);
+      } else {
+        return const Left(MainFailures.serverFailure());
+      }
+    } catch (e) {
+      log(e.toString());
+      return const Left(MainFailures.clientFaliure());
+    }
   }
-  
 }
