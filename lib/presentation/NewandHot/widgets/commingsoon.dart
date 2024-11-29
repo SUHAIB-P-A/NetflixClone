cfaef248403b47cfadc9442a2f267c3cee26139e
thegreatcode1 <suhaibpa72@gmail.com>
2024-11-29 21:41:25 +0530

    add marquee for text scrolling

--- lib/presentation/NewandHot/widgets/commingsoon.dart
+++ lib/presentation/NewandHot/widgets/commingsoon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:netflix/core/colors/colors.dart';
 import 'package:netflix/core/constants/const.dart';
 import 'package:netflix/presentation/NewandHot/widgets/text_widgets.dart';
@@ -64,14 +65,26 @@ class CommigSoonWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Expanded(
-                    child: Text(
-                      movieName,
-                      style: const TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                    child: SizedBox(
+                      height: 40,
+                      child: Marquee(
+                        text: movieName,
+                        numberOfRounds: 1,
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        scrollAxis: Axis.horizontal,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        blankSpace: 250.0,
+                        velocity: 50.0,
+                        pauseAfterRound: const Duration(seconds: 1),
+                        startPadding: 10.0,
+                        accelerationDuration: const Duration(seconds: 1),
+                        accelerationCurve: Curves.linear,
+                        decelerationDuration: const Duration(milliseconds: 500),
+                        decelerationCurve: Curves.easeOutCubic,
                       ),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
                     ),
                   ),
                   const Row(
